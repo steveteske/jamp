e845b9f42b0d527624f54f6bbc83d641143f1f09
Steve Teske <email@steveteske.com>
2022-07-31 15:28:27 -0400

    JAMP - writing to confluence via and dumping the page content.

--- jamp/confluence.py
+++ jamp/confluence.py
@@ -1,6 +1,6 @@
 from atlassian import Confluence
 from auth import Credential
-
+from pprint import pprint
 
 class JampConfluence:
 
@@ -11,9 +11,13 @@ class JampConfluence:
             username=self._cred.username,
             password=self._cred.password)
 
-    def read(self, space: str, page: str):
+
+    def read(self, space: str, page: str, dump=False):
         page = self._confluence.get_page_by_title(space, page)
         print(page)
+        page_id = page['id']
+        full_page = self._confluence.get_page_by_id(page_id,expand="body.storage")
+        pprint(full_page)
 
     def attach(self, space: str, page: str, filename: str):
         self._confluence.attach_file(filename=filename,space=space,title=page)
